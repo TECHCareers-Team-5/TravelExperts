36e977919e0421fd85c1143bdfdf237d481bb1a9
andyzhu7568 <andyzhu7568@gmail.com>
2021-03-20 19:36:45 -0600

    Converted to Pug & functional register database

--- app.js
+++ app.js
@@ -1,66 +1,62 @@
 var createError = require("http-errors");
-var express = require("express");
+const express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+const pug = require("pug");
+
+const url = "mongodb://localhost:27017";
+
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
-const mongoose = require("mongoose");
-
-var app = express();
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set("view engine", "pug");
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-app.use(function (req, res, next) {
-  next(createError(404));
+// catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
+
+// error handlern
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
+
+const { User } = require("./models/users");
+
+app.post("/register", (req, res) => {
+  console.log("Hello");
+
+  const user = new User(req.body);
+  console.log(user);
+  user.save((err) => {
+    if (err) throw err;
+    res.redirect("/thankyou");
+  });
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+app.use((req, res, next) => {
+  res.status(404).render("error");
 });
-
-//Setup Mongoose and connect to DB
-mongoose.connect("mongodb://localhost:27017/registerDB", {
-  useNewUrlParser: true,
-});
-
-const userSchema = new mongoose.Schema({
-  fname: String,
-  lname: String,
-  email: String,
-  passwork: String,
-});
-
-const User = mongoose.model("User", userSchema);
-
-//Send data to DB
-const user = new User({
-  fname: "Andy",
-  lname: "HAHA",
-  email: "abc@gmail.com",
-  password: "123456",
-});
-
-user.save();
 
 module.exports = app;
