972c5d933135e27263aa9399e17db0de54fc8496
GrantvanB <gvanbo@gmail.com>
2021-03-22 22:43:23 -0600

    vacation package sheets converted to pug with seperate stylesheet, register router updated

--- routes/packages.js
+++ routes/packages.js
@@ -6,8 +6,20 @@ router.get("/", function (req, res, next) {
   res.render("packages", { title: "Packages" });
 });
 
-router.get("/:asian", function (req, res, next) {
+router.get("/asian", function (req, res, next) {
   res.render("asia", { title: "Asia" });
+});
+
+router.get("/carribean", function (req, res, next) {
+  res.render("carribean", { title: "Carribean" });
+});
+
+router.get("/polynesia", function (req, res, next) {
+  res.render("polynesia", { title: "Polynesia" });
+});
+
+router.get("/europe", function (req, res, next) {
+  res.render("europe", { title: "Europe" });
 });
 
 module.exports = router;
